b98fb8a95165bbea09b7d5d72fa614c84ce0c885
Franek <kamieniecki.franek@gmail.com>
2020-12-17 11:14:48 +0100

    Button i appendChild

--- script.js
+++ script.js
@@ -17,5 +17,27 @@ const prepreDOMElements = () => {
 const prepareDOMEvents = () => {
 
 }
+const createTooIsArea = () => {
+    const toolsPanel = document.createElement('div')
+    toolsPanel.classList.add('tools')
+    $newTask.appendChild(toolsPanel)
+
+    const completeBtn = document.createElement('button ')
+    completeBtn.classList.add('complete')
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit')
+    editBtn.innerText = 'EDIT'
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+    toolsPanel.appendChild(completeBtn)
+    toolsPanel.appendChild(editBtn)
+    toolsPanel.appendChild(deleteBtn)
+
+
+}
 
 document.addEventListener('DOMContentLoaded', main)
