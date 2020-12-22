0a0122479c156d0a0375d619bf92ebc4013f6b1d
Franek <kamieniecki.franek@gmail.com>
2020-12-22 14:52:17 +0100

    append child

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ const addNewTask = () => {
 
 }
 
+
 document.addEventListener('DOMContentLoaded', main)
 
 
@@ -61,25 +62,23 @@ document.addEventListener('DOMContentLoaded', main)
 
 
 
-// const createTooIsArea = () => {
-//     const toolsPanel = document.createElement('div')
-//     toolsPanel.classList.add('tools')
-//     $newTask.appendChild(toolsPanel)
+const createTooIsArea = () => {
+    const toolsPanel = document.createElement('div')
+    toolsPanel.classList.add('tools')
+    $newTask.appendChild(toolsPanel)
+    const completeBtn = document.createElement('button ')
+    completeBtn.classList.add('complete')
 
-//     const completeBtn = document.createElement('button ')
-//     completeBtn.classList.add('complete')
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit')
+    editBtn.innerText = 'EDIT'
 
-//     const editBtn = document.createElement('button')
-//     editBtn.classList.add('edit')
-//     editBtn.innerText = 'EDIT'
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
 
-//     const deleteBtn = document.createElement('button')
-//     deleteBtn.classList.add('delete')
-//     deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
-
-//     toolsPanel.appendChild(completeBtn)
-//     toolsPanel.appendChild(editBtn)
-//     toolsPanel.appendChild(deleteBtn)
+    toolsPanel.appendChild(completeBtn)
+    toolsPanel.appendChild(editBtn)
+    toolsPanel.appendChild(deleteBtn)
 
 
-// }
